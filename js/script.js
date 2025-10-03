3cef4b81078ad7678f93b7c5a42b288269c4a769
장세융 <seyoong@jangseyung-ui-MacBookPro.local>
2025-10-03 23:15:22 +0900

    모바일 환경 패치

--- js/script.js
+++ js/script.js
@@ -15,16 +15,19 @@ $(document).ready(function(){
         }
     });
 
-    // 3. 네비게이션 메뉴 클릭 시 부드러운 스크롤 이동
+    // 3. 네비게이션 메뉴 클릭 시 부드러운 스크롤 이동 (고정 내비 오프셋 보정)
     $('.nav-link').on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){ // 800ms 속도로 스크롤
-                // window.location.hash = hash; // URL에 #id 추가 (선택사항)
-            });
+            var $target = $(hash);
+            if ($target.length) {
+                var navHeight = $('.navbar').outerHeight() || 0;
+                // 초소형 기기에서는 네비가 2줄이 될 수 있어 여유값 +8
+                var extra = window.matchMedia('(max-width: 576px)').matches ? 8 : 0;
+                var top = $target.offset().top - navHeight - extra;
+                $('html, body').animate({ scrollTop: top }, 800);
+            }
         }
     });
 
