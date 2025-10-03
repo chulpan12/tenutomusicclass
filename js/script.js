df23520be2c85aad3a7137c6c7edd17e8d1d413b
장세융 <seyoong@jangseyung-ui-MacBookPro.local>
2025-10-03 22:55:10 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    // 1. AOS (Animate on Scroll) 라이브러리 초기화
+    AOS.init({
+        duration: 1000, // 애니메이션 지속 시간
+        once: true,     // 애니메이션이 한 번만 실행되도록 설정
+    });
+
+    // 2. 스크롤 시 네비게이션 바 스타일 변경
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    });
+
+    // 3. 네비게이션 메뉴 클릭 시 부드러운 스크롤 이동
+    $('.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){ // 800ms 속도로 스크롤
+                // window.location.hash = hash; // URL에 #id 추가 (선택사항)
+            });
+        }
+    });
+
+    // 4. 모바일 메뉴에서 링크 클릭 시 메뉴 닫기
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+
+});
